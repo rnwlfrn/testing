17a5b87b8341e278114048462d802540a8e66f02
rnwlfrn <hachaiyun12@gmail.com>
2025-03-26 18:14:31 +1300

    Create BeginnersProblems1.py

---
+++ BeginnersProblems1.py
@@ -0,0 +1,11 @@
+# 1. convert minutes into seconds
+a = int(input("enter your value in minutes: "))
+print(a*60, "second")
+
+# 2. minimum cards
+b = int(input("Enter the number of cards Cruella has: "))
+print("Jane receives at least", b*2, "cards")
+
+# 3. Age to days
+c = int(input("Enter your age in years: "))
+print("You are", c*365, "days old.")
